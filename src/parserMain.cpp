1abf574dd12517ee2225daedb42e4a41a1a28353
Maor RG <maor.roizman@utec.edu.pe>
2021-06-01 08:23:31 -0500

    Update parserMain.cpp

--- src/parserMain.cpp
+++ src/parserMain.cpp
@@ -9,7 +9,7 @@ const string FOLDER_PATH = "graphic-interface/src/public/files/monitor/";
 int main()
 {
     QueryParser *qp = new QueryParser(FOLDER_PATH);
-    FileWatcher  fw{FOLDER_PATH, chrono::milliseconds(1000)};
+    FileWatcher  fw{FOLDER_PATH, chrono::milliseconds(100)};
     fw.start([qp](string path_to_watch, FileStatus status) -> void {
         if (!filesystem::is_regular_file(
                 std::filesystem::path(path_to_watch)) &&
